4c0ff4823dd59367da436ed754b9f240cf961141
soc33 <soc33@msn.com>
2019-04-27 23:58:58 -0400

    last try for now

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 //build mode
 let url = process.env.MONGOLAB_URI
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/todos");
+mongoose.connect(process.env.MONGO_URL || "mongodb://127.0.0.1:27017/todos");
 // mongoose.connect(url, { useNewUrlParser: true });
 const connection = mongoose.connection;
 
